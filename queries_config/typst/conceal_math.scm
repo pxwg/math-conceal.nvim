e2c3feb52aa3a6669c3cc71d7b25979829bd299b
pxwg <xiangxy23@mails.tsinghua.edu.cn>
2025-07-16 13:52:02 +0800

    feat: conceal `abs(xxx)` as `|xxx|` in typst

--- queries_config/typst/conceal_math.scm
+++ queries_config/typst/conceal_math.scm
@@ -41,7 +41,8 @@
 ((call
   item: (ident) @_frac_name
   (#eq? @_frac_name "frac"))
- (#set! conceal "" @_frac_name))
+  (#set! conceal "" @_frac_name)
+  (#set! priority 1000))
 
 ; Replace comma with division slash
 ((call
@@ -50,3 +51,21 @@
   (_) .  "," @_comma
   (_))
  (#set! conceal "/" @_comma))
+
+; Conceal "abs" function name
+((call
+  item: (ident) @abs_name
+  (#eq? @abs_name "abs")
+  (#set! conceal "")
+  (#set! priority 100)))
+
+; Conceal parentheses for abs function
+((call
+  item: (ident) @func_name
+  "(" @left_paren
+  (_)
+  ")" @right_paren
+  (#eq? @func_name "abs")
+  (#set! conceal "|" @left_paren)
+  (#set! conceal "|" @right_paren)
+  (#set! priority 90)))
