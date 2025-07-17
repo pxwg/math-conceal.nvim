f3ce9952fc4e172a11a74781ea39e80a46d68503
pxwg <xiangxy23@mails.tsinghua.edu.cn>
2025-07-17 19:57:16 +0800

    feat: conceal `a^(1)` and `a^1` as unicode

--- queries_config/typst/conceal_script.scm
+++ queries_config/typst/conceal_script.scm
@@ -1,40 +1,84 @@
 ; Typst script style conceals
 ; Superscript and subscript conceals
 (attach
-  sup: (letter) @sup_letter
-  (#has-ancestor? @sup_letter math formula)
-  (#set! priority 110)
-  (#lua_func! @sup_letter "sup"))
+  (_)
+  sup: (letter) @sup_object
+  (#has-ancestor? @sup_object math formula)
+  (#set! priority 99)
+  (#lua_func! @sup_object "sup"))
 
 (attach
-  sup: (number) @sup_number
-  (#has-ancestor? @sup_number math formula)
-  (#set! priority 110)
-  (#lua_func! @sup_number "sup"))
+  (_)
+  sup: (number) @sup_object
+  (#has-ancestor? @sup_object math formula)
+  (#any-of? @sup_object "1" "2" "3" "4" "5" "6" "7" "8" "9")
+  (#set! priority 99)
+  (#lua_func! @sup_object "sup"))
 
 (attach
-  sup: (ident) @sup_ident
-  (#has-ancestor? @sup_ident math formula)
-  (#set! priority 110)
-  (#lua_func! @sup_ident "sup"))
+  (_)
+  sup: (ident) @sup_object
+  (#has-ancestor? @sup_object math formula)
+  (#set! priority 99)
+  (#lua_func! @sup_object "sup"))
 
 (attach
-  sub: (letter) @sub_letter
-  (#has-ancestor? @sub_letter math formula)
-  (#set! priority 110)
-  (#lua_func! @sub_letter "sub"))
+  (_)
+  "^" @sup_symbol
+  sup: (number) @sup_object
+  (#has-ancestor? @sup_object math formula)
+  (#any-of? @sup_object "1" "2" "3" "4" "5" "6" "7" "8" "9")
+  (#set! priority 98)
+  (#set! conceal "" @sup_symbol))
 
 (attach
-  sub: (number) @sub_number
-  (#has-ancestor? @sub_number math formula)
-  (#set! priority 110)
-  (#lua_func! @sub_number "sub"))
+  (_)
+  "^" @sup_symbol
+  sup: (letter) @sup_object
+  ((#match? @sup_object "[a-z]")
+  (#has-ancestor? @sup_object math formula)
+  (#set! priority 98)
+  (#set! conceal "" @sup_object)))
+
+; Subscript conceals
+(attach
+  (_)
+  sub: (letter) @sub_object
+  (#set! priority 99)
+  (#lua_func! @sub_object "sub"))
 
 (attach
-  sub: (ident) @sub_ident
-  (#has-ancestor? @sub_ident math formula)
-  (#set! priority 110)
-  (#lua_func! @sub_ident "sub"))
+  (_)
+  sub: (number) @sub_object
+  (#has-ancestor? @sub_object math formula)
+  (#any-of? @sub_object "1" "2" "3" "4" "5" "6" "7" "8" "9")
+  (#set! priority 99)
+  (#lua_func! @sub_object "sub"))
+
+(attach
+  (_)
+  sub: (ident) @sub_object
+  (#has-ancestor? @sub_object math formula)
+  (#set! priority 99)
+  (#lua_func! @sub_object "sub"))
+
+(attach
+  (_)
+  "_" @sub_symbol
+  sub: (number) @sub_object
+  (#any-of? @sub_object "1" "2" "3" "4" "5" "6" "7" "8" "9")
+  (#has-ancestor? @sub_object math formula)
+  (#set! priority 98)
+  (#set! conceal "" @sub_symbol))
+
+(attach
+  (_)
+  "_" @sub_symbol
+  sub: (letter) @sub_object
+  ((#match? @sub_object "[aehijklmnoprstuvx]")
+  (#has-ancestor? @sub_object math formula)
+  (#set! priority 98)
+  (#set! conceal "" @sub_symbol)))
 
 ; Script functions like upright, script, etc.
 (call
@@ -44,21 +88,71 @@
   (#set! conceal ""))
 
 ; Capture and conceal the opening parenthesis of the sub/supscript group
+; For superscript with parentheses - hide both ^ and parentheses when content matches criteria
 (math
   (formula
     (attach
       (_)
+      "^" @sup_symbol
       sup: (group
-        "(" @_open_paren)))
-  (#set! conceal "" @_open_paren))
+        "(" @open_paren
+        (formula
+          (letter) @sup_letter)
+        ")" @close_paren)
+      (#match? @sup_letter "[a-z]")
+      (#set! priority 97)
+      (#lua_func! @sup_letter "sup"))))
 
 (math
   (formula
     (attach
       (_)
-      sub: (group
-        "(" @_open_paren)))
-  (#set! conceal "" @_open_paren))
+      "^" @sup_symbol
+      sup: (group
+        "(" @open_paren
+        (formula
+          (letter) @sup_letter)
+        ")" @close_paren)
+      (#match? @sup_letter "[a-z]")
+      (#set! priority 96)
+      (#set! conceal "" @sup_symbol))))
+
+; For superscript with parentheses - hide both ^ and parentheses when content is a number
+(math
+  (formula
+    (attach
+      (_)
+      "^" @sup_symbol
+      sup: (group
+        "(" @open_paren
+        (formula
+          (number) @sup_number)
+        ")" @close_paren)
+      (#any-of? @sup_number "1" "2" "3" "4" "5" "6" "7" "8" "9")
+      (#set! priority 97)
+      (#lua_func! @sup_number "sup"))))
+
+(math
+  (formula
+    (attach
+      (_)
+      "^" @sup_symbol
+      sup: (group
+        "(" @open_paren
+        (formula
+          (number) @sup_number)
+        ")" @close_paren)
+      (#any-of? @sup_number "1" "2" "3" "4" "5" "6" "7" "8" "9")
+      (#set! priority 96)
+      (#set! conceal "" @sup_symbol))))
+
+(math
+  (formula
+    (attach
+      (_)
+      sup: (group
+        "(" @_open_paren))
+    (#set! conceal "" @_open_paren)))
 
 (math
   (formula
@@ -67,6 +161,14 @@
       sup: (group
         ")" @_close_paren)))
   (#set! conceal "" @_close_paren))
+
+(math
+  (formula
+    (attach
+      (_)
+      sub: (group
+        "(" @_open_paren)))
+  (#set! conceal "" @_open_paren))
 
 (math
   (formula
