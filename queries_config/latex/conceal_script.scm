7637a29439929252868fef8405fa3e3e878843d3
Xinyu Xiang <149765160+pxwg@users.noreply.github.com>
2025-07-29 14:54:08 +0800

    fix: conceal node for latex (#42)

--- queries_config/latex/conceal_script.scm
+++ queries_config/latex/conceal_script.scm
@@ -2,12 +2,14 @@
   "_" @sub_symbol
   subscript: (curly_group
     "{" @open_paren
+    (_) @sub_letter
     "}" @close_paren) @sub_object
   (#match? @sub_object "^\\{[aehijklmnoprstuvx1234567890]\\}$")
+  (#set! priority 101)
   (#set! @sub_symbol conceal "")
   (#set! @open_paren conceal "")
   (#set! @close_paren conceal "")
-  (#set-sub! @sub_object))
+  (#set-sub! @sub_letter))
 
 (subscript
   "_" @sub_symbol
@@ -29,12 +31,13 @@
   "^" @sup_symbol
   superscript: (curly_group
     "{" @open_paren
+    (_) @sup_letter
     "}" @close_paren) @sup_object
   (#match? @sup_object "^\\{[a-z1-9]\\}$")
   (#set! @sup_symbol conceal "")
   (#set! @open_paren conceal "")
   (#set! @close_paren conceal "")
-  (#set-sup! @sup_object))
+  (#set-sup! @sup_letter))
 
 (superscript
   "^" @sup_symbol
