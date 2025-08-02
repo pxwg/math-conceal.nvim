39e42d0dc0388aa31b4fc28b656185a4ed980376
pxwg <xiangxy23@mails.tsinghua.edu.cn>
2025-08-02 14:00:03 +0800

    fix: better query for typst

--- queries_config/typst/conceal_phy.scm
+++ queries_config/typst/conceal_phy.scm
@@ -2,20 +2,13 @@
 ; Physics constants and symbols
 (call
   item: (ident) @typ_phy_symbol
-  (#any-of? @typ_phy_symbol
-    "hbar" "planck" "boltzmann" "avogadro" "gas" "electron" "proton" "neutron" "muon" "tau" "charge"
-    "mass" "energy" "momentum" "angular" "spin" "magnetic" "electric" "permittivity" "permeability"
-    "speed" "light" "gravity" "acceleration" "force" "pressure" "temperature" "entropy" "enthalpy"
-    "helmholtz" "gibbs")
+  (#match? @typ_phy_symbol "^(hbar|planck|boltzmann|avogadro|gas|electron|proton|neutron|muon|tau|charge|mass|energy|momentum|angular|spin|magnetic|electric|permittivity|permeability|speed|light|gravity|acceleration|force|pressure|temperature|entropy|enthalpy|helmholtz|gibbs)$")
   ; (#has-ancestor? @typ_phy_symbol math formula)
   (#set-conceal! @typ_phy_symbol "conceal"))
 
 ; Physics units and quantities
 ((ident) @typ_phy_symbol
-  (#any-of? @typ_phy_symbol
-    "hbar" "planck" "boltzmann" "avogadro" "electron" "proton" "neutron" "speed" "light" "gravity"
-    "charge" "mass" "energy" "momentum" "angular" "spin" "magnetic" "electric" "force" "pressure"
-    "temperature" "entropy" "enthalpy" "helmholtz" "gibbs")
+  (#match? @typ_phy_symbol "^(hbar|planck|boltzmann|avogadro|electron|proton|neutron|speed|light|gravity|charge|mass|energy|momentum|angular|spin|magnetic|electric|force|pressure|temperature|entropy|enthalpy|helmholtz|gibbs)$")
   ; (#has-ancestor? @typ_phy_symbol math formula)
   (#set-conceal! @typ_phy_symbol "conceal"))
 
