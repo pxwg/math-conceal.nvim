185c4906728397359947cf7645c81351420ba33c
pxwg <xiangxy23@mails.tsinghua.edu.cn>
2025-08-02 13:53:07 +0800

    fix: better query for latex

--- queries_config/latex/conceal_greek.scm
+++ queries_config/latex/conceal_greek.scm
@@ -1,12 +1,12 @@
 ; greek conceal
 (generic_command
-  command: ((command_name) @tex_greek_symbol
-    (#any-of? @tex_greek_symbol
-      "\\alpha" "\\beta" "\\gamma" "\\delta" "\\epsilon" "\\varepsilon" "\\zeta" "\\eta" "\\theta"
-      "\\vartheta" "\\iota" "\\kappa" "\\lambda" "\\mu" "\\nu" "\\xi" "\\pi" "\\varpi" "\\rho"
-      "\\varrho" "\\sigma" "\\varsigma" "\\tau" "\\upsilon" "\\phi" "\\varphi" "\\chi" "\\psi"
-      "\\omega" "\\Gamma" "\\Delta" "\\Theta" "\\Lambda" "\\Xi" "\\Pi" "\\Sigma" "\\Upsilon" "\\Phi"
-      "\\Chi" "\\Psi" "\\Omega"))
-  (#has-ancestor? @tex_greek_symbol math_environment inline_formula displayed_equation)
-  (#not-has-ancestor? @tex_greek_symbol label_definition text_mode)
-  (#set-conceal! @tex_greek_symbol "conceal"))
+  command: ((command_name) @tex_greek
+    (#match? @tex_greek "^\\\\(varalpha|varbeta|vargamma|vardelta|varepsilon|varzeta|vareta|vartheta|variota|varkappa|varlambda|varmu|varnu|varxi|varpi|varrho|varsigma|vartau|varupsilon|varphi|varchi|varpsi|varomega|alpha|beta|gamma|delta|epsilon|zeta|eta|theta|iota|kappa|lambda|mu|nu|xi|pi|rho|sigma|tau|upsilon|phi|chi|psi|omega)$"))
+  ; (#has-parent? @tex_greek math_environment)
+  (#set-conceal! @tex_greek "conceal"))
+
+(generic_command
+  command: ((command_name) @tex_greek
+    (#match? @tex_greek "^\\\\(Gamma|Delta|Theta|Lambda|Xi|Pi|Sigma|Upsilon|Phi|Chi|Psi|Omega)$"))
+  ; (#has-parent? @tex_greek math_environment)
+  (#set-conceal! @tex_greek "conceal"))
