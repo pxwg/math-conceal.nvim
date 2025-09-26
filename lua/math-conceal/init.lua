96dd92bd5b327cdec4a33da6bfa1840c03726564
Xinyu Xiang <149765160+pxwg@users.noreply.github.com>
2025-09-26 11:06:16 +0800

    fix: Speed up plugin startup time (#53)
    * fix: add `diff` symbol
    
    * fix: better sub/supscript for typst
    
    * feat: auto load user-defined symbols by \(re)newcommands
    
    * move query files to a canonical dir
    
    * feat: auto load user-defined symbols by \(re)newcommands
    
    * feat: auto load user-defined symbols by \(re)newcommands
    
    * fix: faster loading

--- lua/math-conceal/init.lua
+++ lua/math-conceal/init.lua
@@ -1,5 +1,6 @@
 local M = {}
 local highlights = require("math-conceal.highlights")
+local queries = require("treesitter_query")
 local utils = require("utils.latex_conceal")
 
 --- TODO: add custum_function setup
@@ -33,6 +34,16 @@ local autocmd = require("math-conceal.autocmd")
 function M.setup(opts)
   highlights.set_highlights()
   M.opts = vim.tbl_deep_extend("force", default_opts, opts or {})
+  local latex_query_files = queries.get_conceal_queries(M.opts).latex
+  local typst_query_files = queries.get_conceal_queries(M.opts).typst
+  local init_data = {
+    latex = latex_query_files,
+    typst = typst_query_files,
+  }
+  local latex_queries = queries.read_query_files(init_data.latex)
+  local typst_queries = queries.read_query_files(init_data.typst)
+  init_data.latex_queries = latex_queries
+  init_data.typst_queries = typst_queries
   if not utils.ensure_loaded() then
     vim.notify(
       "Failed to load math-conceal library. Make sure you run 'make lua51' or 'make luajit' first.",
@@ -45,7 +56,7 @@ function M.setup(opts)
     if not success then
       vim.notify("LaTeX conceal initialization failed", vim.log.levels.WARN)
     else
-      autocmd.subscribe_autocmd(M.opts)
+      autocmd.subscribe_autocmd(M.opts, init_data)
     end
   end
 end
