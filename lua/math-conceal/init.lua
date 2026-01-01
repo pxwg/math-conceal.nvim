721122f28ffdd4f09fa30840dbf778696ea53be1
Wu, Zhenyu <wuzhenyu@ustc.edu>
2026-01-02 02:21:47 +0800

    feat: create autocmd BufReadPost

--- lua/math-conceal/init.lua
+++ lua/math-conceal/init.lua
@@ -58,6 +58,7 @@ local M = {
 --- @field conceal string[]?: Enable or disable math symbol concealment. You can add your own custom conceal types here. Default is {"greek", "script", "math", "font", "delim"}.
 --- @field ft string[]: A list of filetypes to enable LaTeX conceal
 --- @field depth integer
+--- @field augroup_id integer?
 --- @field ns_id integer
 --- @field highlights table<string, table<string, string>>
 
@@ -67,50 +68,69 @@ function M.setup(opts)
   M.opts = vim.tbl_deep_extend("force", M.opts, opts or {})
 end
 
----set math conceal
+---set highlights only when `filetype` is in `M.opts.ft`
 ---@param filetype string?
 function M.set(filetype)
   filetype = filetype or vim.bo.filetype
   for _, ft in ipairs(M.opts.ft) do
     if ft == filetype then
-      M.set_highlights(filetype, M.opts.ns_id, M.opts.highlights, M.opts.conceal)
-      return
+      M.set_hl(filetype)
     end
+  end
+end
+
+---set highlights only once
+---@param filetype string?
+function M.set_hl(filetype)
+  --- first run
+  if #M.queries == 0 then
+    for name, val in pairs(M.opts.highlights) do
+      vim.api.nvim_set_hl(M.opts.ns_id, name, val)
+    end
+    queries.load_queries()
+  end
+
+  --- after editing preamble and save, reset highlights
+  if filetype == "tex" then
+    M.opts.augroup_id = M.opts.augroup_id or vim.api.nvim_create_augroup("math-conceal", {})
+    vim.api.nvim_create_autocmd("BufWritePost", {
+      group = M.opts.augroup_id,
+      buffer = 0,
+      callback = function()
+        M.set_highlights(filetype, M.queries.latex)
+        vim.treesitter.start()
+      end
+    })
+  end
+
+  -- set typst math conceal for typst
+  -- and set latex math conceal for all other filetypes.
+  local ft = filetype == "typst" and filetype or "latex"
+  ---always reset highlights for tex due to preamble
+  local should_set_hl = filetype == "tex"
+  -- if haven't set highlights, must set highlights
+  if M.queries[ft] == nil then
+    M.files[ft] = queries.get_conceal_queries(ft, M.opts.conceal)
+    M.queries[ft] = queries.read_query_files(M.files[ft])
+    should_set_hl = true
+  end
+  if should_set_hl then
+    M.set_highlights(filetype, M.queries[ft])
   end
 end
 
 ---set highlights
 ---@param filetype string?
----@param ns_id integer?
----@param highlights table<string, table<string, string>>
----@param conceal string[]
-function M.set_highlights(filetype, ns_id, highlights, conceal)
+---@param code string?
+function M.set_highlights(filetype, code)
   filetype = filetype or vim.bo.filetype
-  ns_id = ns_id or 0
-  highlights = highlights or {}
-  conceal = conceal or {}
-  local code = ""
+  code = code or ""
+
   if filetype == "tex" then
     local conceal_map = queries.get_preamble_conceal_map()
-    code = queries.update_latex_queries(conceal_map)
+    code = code .. "\n" .. queries.update_latex_queries(conceal_map)
   end
-  if filetype ~= "typst" then
-    filetype = "latex"
-  end
-
-  if #M.queries == 0 then
-    for name, val in pairs(highlights) do
-      vim.api.nvim_set_hl(ns_id, name, val)
-    end
-    queries.load_queries()
-  end
-
-  if M.queries[filetype] == nil then
-    M.files[filetype] = queries.get_conceal_queries(filetype, conceal)
-    M.queries[filetype] = queries.read_query_files(M.files[filetype])
-  end
-  code = M.queries[filetype] .. "\n" .. code
-  vim.treesitter.query.set(filetype, "highlights", code)
+  vim.treesitter.query.set(ft, "highlights", code)
 end
 
 return M
