5db73e903c5b32c83214836a1a83193ef6597109
Wu, Zhenyu <wuzhenyu@ustc.edu>
2026-01-01 19:53:19 +0800

    chore: Allow users to set conceallevel=1 by themselves

--- lua/math-conceal/autocmd.lua
+++ lua/math-conceal/autocmd.lua
@@ -1,38 +1,21 @@
 local M = {}
-local autocmd = vim.api.nvim_create_autocmd
 local queries = require("math-conceal.query")
 
 --- @param opts LaTeXConcealOptions
 --- @param augroup_id integer?
-local function subscribe_autocmd(opts, init_data, augroup_id)
-  if not opts.enabled then
-    return
-  end
-
+function M.subscribe_autocmd(opts, init_data, augroup_id)
   augroup_id = augroup_id or vim.api.nvim_create_augroup("math-conceal", {})
-  local ft = opts.ft
-  autocmd({ "BufEnter", "BufReadPre" }, {
+  vim.api.nvim_create_autocmd("BufReadPost", {
     group = augroup_id,
-    pattern = ft,
-    callback = function()
-      vim.opt_local.conceallevel = 2
-      -- No need to reload queries on every buffer event
-    end,
-  })
-  autocmd("BufReadPost", {
-    group = augroup_id,
-    pattern = ft,
+    pattern = opts.ft,
     callback = function()
       queries.load_queries(opts, init_data)
       local conceal_map = queries.get_preamble_conceal_map()
       if vim.bo.filetype == "tex" then
         queries.update_latex_queries(conceal_map, opts)
       end
-      vim.cmd("e")
     end,
   })
 end
-
-M.subscribe_autocmd = subscribe_autocmd
 
 return M
