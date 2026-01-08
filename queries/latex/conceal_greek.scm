5b6b228480f3d57dcd054cfac10401bf5b1109db
pxwg <xiangxy23@mails.tsinghua.edu.cn>
2026-01-09 00:13:16 +0800

    fix: remove queries

--- queries/latex/conceal_greek.scm
+++
@@ -1,5 +0,0 @@
-; greek conceal - regex removed, Rust hash table will filter
-(generic_command
-  command: (command_name) @tex_greek
-  ; (#has-parent? @tex_greek math_environment)
-  (#set-conceal! @tex_greek "conceal"))
