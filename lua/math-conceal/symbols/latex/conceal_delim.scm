3eb08832acd1312e257c658788da4a2f20d997c5
yorik1984 <yorik1984@gmail.com>
2026-05-29 01:34:08 +0300

    feat(latex-math): improve fraction conceal patterns
    - Add more conditions for matching fraction folding patterns
    - Replace symbols for better visual distinction between simple and complex division
    - Restrict all operations to math environment only
    - Auto-format code
    - Successfully tested on control expressions

--- lua/math-conceal/symbols/latex/conceal_delim.scm
+++ lua/math-conceal/symbols/latex/conceal_delim.scm
@@ -1,16 +1,31 @@
 (curly_group
-  "{" @conceal
+  [
+    "{"
+    "}"
+  ] @conceal
   (#not-has-grandparent? @conceal
     title_declaration author_declaration chapter part section subsection subsubsection paragraph
     subparagraph command generic_command subscript superscript)
   (#set! conceal ""))
 
-(curly_group
-  "}" @conceal
-  (#not-has-grandparent? @conceal
-    title_declaration author_declaration chapter part section subsection subsubsection paragraph
-    subparagraph command generic_command subscript superscript)
-  (#set! conceal ""))
+; Hints to simplify concealing
+; \frac{{long_expression_1}}{{long_expression_2}}
+;       ^               ^    ^                 ^
+; conceal inner to
+; {(long_expression)}
+; \frac{{1+1}}{{2+2}} -> (1+1)/(2+2)
+(generic_command
+  command: (command_name) @frac
+  (curly_group
+    .
+    (curly_group
+      [
+        "{" @left_1
+        "}" @right_1
+      ]) .)
+  (#any-of? @frac "\\frac" "\\dfrac" "\\tfrac" "\\cfrac")
+  (#set! @left_1 conceal "(")
+  (#set! @right_1 conceal ")"))
 
 (math_delimiter
   left_command: _ @conceal
@@ -31,8 +46,10 @@
   (#set! @conceal_dollar conceal ""))
 
 (displayed_equation
-  "\\[" @conceal_dollar
-  "\\]" @conceal_dollar
+  [
+    "\\["
+    "\\]"
+  ] @conceal_dollar
   (#set! @conceal_dollar conceal ""))
 
 (displayed_equation
@@ -46,6 +63,9 @@
 
 ("\\item" @punctuation.special @conceal
   (#set! conceal "○"))
+
+("\\item*" @punctuation.special @conceal
+  (#set! conceal "●"))
 
 ((text
   word: (delimiter) @conceal)
