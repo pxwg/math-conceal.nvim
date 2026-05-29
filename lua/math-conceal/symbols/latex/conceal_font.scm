64730d4b3c78667436be329ab17efd500edf2dd5
yorik1984 <yorik1984@gmail.com>
2026-05-29 03:54:59 +0300

    feat(latex-font): extend text commands with support for external packages
    - Expand list of text commands, including support for external packages
    - Each command now hides only where it actually works
    - Tested on examples

--- lua/math-conceal/symbols/latex/conceal_font.scm
+++ lua/math-conceal/symbols/latex/conceal_font.scm
@@ -1,15 +1,90 @@
 (generic_command
-  command: (command_name) @conceal
-  (#any-of? @conceal "\\emph" "\\mathit" "\\textit" "\\mathbf" "\\textbf" "\\mathbb" "\\mathcal" "\\mathfrak" "\\mathscr" "\\mathsf" "\\mathrm")
-  (#set! conceal ""))
-
-((generic_command
   command: (command_name) @tex_font_name
   arg: (curly_group
-    "{" @left_paren
-    (_)
-    "}" @right_paren))
-  (#any-of? @tex_font_name "\\emph" "\\mathit" "\\textit" "\\mathbf" "\\textbf" "\\mathbb" "\\mathcal" "\\mathfrak" "\\mathscr" "\\mathsf" "\\mathrm")
+    [
+      "{" @left_paren
+      "}" @right_paren
+    ])
+  (#any-of? @tex_font_name
+    "\\textbf" "\\textit" "\\textlf" "\\textmd" "\\textrm" "\\textsc" "\\textsl" "\\textsf"
+    "\\texttc" "\\texttt" "\\textulc" "\\textup" "\\textnormal" "\\emph" "\\rmfamily" "\\sffamily"
+    "\\ttfamily" "\\itshape" "\\scshape" "\\slshape" "\\upshape" "\\bfseries" "\\mdseries")
+  (#not-has-ancestor? @tex_font_name displayed_equation inline_formula math_environment)
+  (#set! @left_paren conceal "")
+  (#set! @right_paren conceal "")
+  (#set! @tex_font_name conceal ""))
+
+(generic_command
+  command: (command_name) @tex_font_name
+  arg: (curly_group
+    [
+      "{" @left_paren
+      "}" @right_paren
+    ])
+  (#any-of? @tex_font_name
+    "\\textbf" "\\textit" "\\textlf" "\\textmd" "\\textrm" "\\textsc" "\\textsl" "\\textsf"
+    "\\texttc" "\\texttt" "\\textulc" "\\textup" "\\textnormal" "\\emph" "\\rmfamily" "\\sffamily"
+    "\\ttfamily" "\\itshape" "\\scshape" "\\slshape" "\\upshape" "\\bfseries" "\\mdseries")
+  (#has-ancestor? @tex_font_name text_mode)
+  (#set! @left_paren conceal "")
+  (#set! @right_paren conceal "")
+  (#set! conceal ""))
+
+(generic_command
+  command: (command_name) @tex_font_name
+  (#any-of? @tex_font_name
+    "\\tiny" "\\scriptsize" "\\footnotesize" "\\normalsize" "\\small" "\\large" "\\Large" "\\LARGE"
+    "\\huge" "\\Huge" "\\normalfont")
+  (#not-has-ancestor? @tex_font_name displayed_equation inline_formula math_environment)
+  (#set! @tex_font_name conceal ""))
+
+(generic_command
+  command: (command_name) @tex_font_name
+  (#any-of? @tex_font_name
+    "\\tiny" "\\scriptsize" "\\footnotesize" "\\normalsize" "\\small" "\\large" "\\Large" "\\LARGE"
+    "\\huge" "\\Huge" "\\normalfont")
+  (#has-ancestor? @tex_font_name text_mode)
+  (#set! conceal ""))
+
+(text_mode
+  command: "\\text" @tex_font_name
+  content: (curly_group
+    [
+      "{" @left_paren
+      "}" @right_paren
+    ])
+  (#has-ancestor? @tex_font_name displayed_equation inline_formula math_environment)
+  (#set! @left_paren conceal "")
+  (#set! @right_paren conceal "")
+  (#set! @tex_font_name conceal ""))
+
+(text_mode
+  command: [
+    "\\intertext"
+    "\\shortintertext"
+  ] @tex_font_name
+  content: (curly_group
+    [
+      "{" @left_paren
+      "}" @right_paren
+    ])
+  (#has-ancestor? @tex_font_name math_environment)
+  (#set! @left_paren conceal "")
+  (#set! @right_paren conceal "")
+  (#set! @tex_font_name conceal ""))
+
+(generic_command
+  command: (command_name) @tex_font_name
+  arg: (curly_group
+    [
+      "{" @left_paren
+      "}" @right_paren
+    ])
+  (#any-of? @tex_font_name
+    "\\mathrm" "\\mathtt" "\\mathsf" "\\mathnormal" "\\mathbf" "\\mathit" "\\mathcal" "\\mathbfit"
+    "\\mathbb" "\\mathfrak" "\\mathscr" "\\mathds" "\\mathbbm" "\\mathbbb")
+  (#has-ancestor? @tex_font_name displayed_equation inline_formula math_environment)
+  (#not-has-ancestor? @tex_font_name text_mode)
   (#set! @left_paren conceal "")
   (#set! @right_paren conceal "")
   (#set! @tex_font_name conceal ""))
@@ -21,6 +96,8 @@
     (text
       word: (word) @font_letter)
     "}" @right_paren))
+  (#has-ancestor? @tex_font_name displayed_equation inline_formula math_environment)
+  (#not-has-ancestor? @tex_font_name text_mode)
   (#any-of? @tex_font_name "\\mathbb" "\\mathcal" "\\mathfrak" "\\mathscr" "\\mathsf" "\\mathrm")
   (#set! @left_paren conceal "")
   (#set! @right_paren conceal "")
