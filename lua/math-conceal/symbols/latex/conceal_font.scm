e0344c59eedea5bab038e30ea30ecdda600a1b81
yorik1984 <yorik1984@gmail.com>
2026-05-31 09:37:47 +0300

    feat(latex-fonts): Add unicode-math font commands + glyph hiding for `\sym...`, `\math...`, `\bm`
    - Added commands (≈40) from the `unicode-math` package for full font support, including aliases and fallbacks for legacy commands.
    
    - Added support for hiding Latin and Greek letters as commands for the `\sym...` symbolic command family.
    
    - For `\math...` commands — hiding support for Latin letters only (no Greek).
    
    - Added hiding for Latin and Greek letters for the `\bm` command to ensure compatibility with pdflatex.
    
    - All glyphs were thoroughly tested across multiple files to ensure visual consistency with compiled documents.

--- lua/math-conceal/symbols/latex/conceal_font.scm
+++ lua/math-conceal/symbols/latex/conceal_font.scm
@@ -6,9 +6,9 @@
       "}" @right_paren
     ])
   (#any-of? @tex_font_name
-    "\\textbf" "\\textit" "\\textlf" "\\textmd" "\\textrm" "\\textsc" "\\textsl" "\\textsf"
-    "\\texttc" "\\texttt" "\\textulc" "\\textup" "\\textnormal" "\\emph" "\\rmfamily" "\\sffamily"
-    "\\ttfamily" "\\itshape" "\\scshape" "\\slshape" "\\upshape" "\\bfseries" "\\mdseries")
+    "\\bfseries" "\\emph" "\\itshape" "\\mdseries" "\\rmfamily" "\\scshape" "\\sffamily" "\\slshape"
+    "\\textbf" "\\textit" "\\textlf" "\\textmd" "\\textnormal" "\\textrm" "\\textsc" "\\textsf"
+    "\\textsl" "\\texttc" "\\texttt" "\\textulc" "\\textup" "\\ttfamily" "\\upshape")
   (#not-has-ancestor? @tex_font_name displayed_equation inline_formula math_environment)
   (#set! @left_paren conceal "")
   (#set! @right_paren conceal "")
@@ -22,9 +22,9 @@
       "}" @right_paren
     ])
   (#any-of? @tex_font_name
-    "\\textbf" "\\textit" "\\textlf" "\\textmd" "\\textrm" "\\textsc" "\\textsl" "\\textsf"
-    "\\texttc" "\\texttt" "\\textulc" "\\textup" "\\textnormal" "\\emph" "\\rmfamily" "\\sffamily"
-    "\\ttfamily" "\\itshape" "\\scshape" "\\slshape" "\\upshape" "\\bfseries" "\\mdseries")
+    "\\bfseries" "\\emph" "\\itshape" "\\mdseries" "\\rmfamily" "\\scshape" "\\sffamily" "\\slshape"
+    "\\textbf" "\\textit" "\\textlf" "\\textmd" "\\textnormal" "\\textrm" "\\textsc" "\\textsf"
+    "\\textsl" "\\texttc" "\\texttt" "\\textulc" "\\textup" "\\ttfamily" "\\upshape")
   (#has-ancestor? @tex_font_name text_mode)
   (#set! @left_paren conceal "")
   (#set! @right_paren conceal "")
@@ -33,16 +33,16 @@
 (generic_command
   command: (command_name) @tex_font_name
   (#any-of? @tex_font_name
-    "\\tiny" "\\scriptsize" "\\footnotesize" "\\normalsize" "\\small" "\\large" "\\Large" "\\LARGE"
-    "\\huge" "\\Huge" "\\normalfont")
+    "\\Huge" "\\LARGE" "\\Large" "\\footnotesize" "\\huge" "\\large" "\\normalfont" "\\normalsize"
+    "\\scriptsize" "\\small" "\\tiny")
   (#not-has-ancestor? @tex_font_name displayed_equation inline_formula math_environment)
   (#set! @tex_font_name conceal ""))
 
 (generic_command
   command: (command_name) @tex_font_name
   (#any-of? @tex_font_name
-    "\\tiny" "\\scriptsize" "\\footnotesize" "\\normalsize" "\\small" "\\large" "\\Large" "\\LARGE"
-    "\\huge" "\\Huge" "\\normalfont")
+    "\\Huge" "\\LARGE" "\\Large" "\\footnotesize" "\\huge" "\\large" "\\normalfont" "\\normalsize"
+    "\\scriptsize" "\\small" "\\tiny")
   (#has-ancestor? @tex_font_name text_mode)
   (#set! conceal ""))
 
@@ -73,6 +73,7 @@
   (#set! @right_paren conceal "")
   (#set! @tex_font_name conceal ""))
 
+; full math-font commans list. Work with pdflatex and unicode-math
 (generic_command
   command: (command_name) @tex_font_name
   arg: (curly_group
@@ -81,37 +82,50 @@
       "}" @right_paren
     ])
   (#any-of? @tex_font_name
-    "\\mathrm" "\\mathtt" "\\mathsf" "\\mathnormal" "\\mathbf" "\\mathit" "\\mathcal" "\\mathbfit"
-    "\\mathbb" "\\mathfrak" "\\mathscr" "\\mathds" "\\mathbbm" "\\mathbbb")
+    "\\mathnormal" "\\mathrm" "\\mathit" "\\mathbf" "\\mathsf" "\\mathtt" "\\mathcal" "\\mathscr"
+    "\\mathbb" "\\mathfrak" "\\bm" "\\symup" "\\symit" "\\symbf" "\\symsf" "\\symtt" "\\symbfup"
+    "\\symbfit" "\\symsfup" "\\symsfit" "\\symbfsf" "\\symbfsfup" "\\symbfsfit" "\\symcal"
+    "\\symbfcal" "\\symscr" "\\symbfscr" "\\symbb" "\\symbbit" "\\symfrak" "\\symbffrak" "\\mathup"
+    "\\mathbfup" "\\mathbfit" "\\mathsfup" "\\mathsfit" "\\mathbfsf" "\\mathbfsfup" "\\mathbfsfit"
+    "\\mathbfcal" "\\mathbfscr" "\\mathbbit" "\\mathbffrak")
   (#has-ancestor? @tex_font_name displayed_equation inline_formula math_environment)
   (#not-has-ancestor? @tex_font_name text_mode)
   (#set! @left_paren conceal "")
   (#set! @right_paren conceal "")
   (#set! @tex_font_name conceal ""))
 
-((generic_command
+; some commands are alias from unicode-math but also work with pdflatex. Conceal only with latin letters
+(generic_command
   command: (command_name) @tex_font_name
   arg: (curly_group
     "{" @left_paren
     (text
       word: (word) @font_letter)
-    "}" @right_paren))
+    "}" @right_paren)
   (#has-ancestor? @tex_font_name displayed_equation inline_formula math_environment)
   (#not-has-ancestor? @tex_font_name text_mode)
-  (#any-of? @tex_font_name "\\mathbb" "\\mathcal" "\\mathfrak" "\\mathscr" "\\mathsf" "\\mathrm")
+  (#any-of? @tex_font_name
+    "\\mathrm" "\\mathit" "\\mathbf" "\\mathsf" "\\mathtt" "\\mathcal" "\\mathscr" "\\mathbb"
+    "\\mathfrak" "\\mathup" "\\mathbfup" "\\mathbfit" "\\mathsfup" "\\mathsfit" "\\mathbfsf"
+    "\\mathbfsfup" "\\mathbfsfit" "\\mathbfcal" "\\mathbfscr" "\\mathbbit" "\\mathbffrak")
   (#set! @left_paren conceal "")
   (#set! @right_paren conceal "")
   (#set! @tex_font_name conceal "")
   ; Regex removed - Rust hash table will filter valid font letters
   (#set-font! @font_letter @tex_font_name))
 
-((generic_command
+; conceal letters and greek letters commands
+(generic_command
   command: (command_name) @tex_font_name
   arg: (curly_group
     "{" @left_paren
     (_) @font_letter
-    "}" @right_paren))
-  (#any-of? @tex_font_name "\\bar" "\\widetilde" "\\hat" "\\dot" "\\ddot")
+    "}" @right_paren)
+  (#any-of? @tex_font_name
+    "\\bar" "\\widetilde" "\\hat" "\\dot" "\\ddot" "\\bm" "\\symup" "\\symit" "\\symbf" "\\symsf"
+    "\\symtt" "\\symbfup" "\\symbfit" "\\symsfup" "\\symsfit" "\\symbfsf" "\\symbfsfup"
+    "\\symbfsfit" "\\symcal" "\\symbfcal" "\\symscr" "\\symbfscr" "\\symbb" "\\symbbit" "\\symfrak"
+    "\\symbffrak")
   (#set! @left_paren conceal "")
   (#set! @right_paren conceal "")
   (#set! @tex_font_name conceal "")
