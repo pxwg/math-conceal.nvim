03e2e616392ae1fa0d0a8ef0e24427ce684c4daa
yorik1984 <yorik1984@gmail.com>
2026-05-31 22:06:19 +0300

    Enhance LaTeX math, symbols, and font handling for readability (#142)
    * feat(latex-math): improve fraction conceal patterns
    
    - Add more conditions for matching fraction folding patterns
    - Replace symbols for better visual distinction between simple and complex division
    - Restrict all operations to math environment only
    - Auto-format code
    - Successfully tested on control expressions
    
    * feat(latex-script): enhance subscript and superscript conceal patterns
    
    - Add more conditions for matching conceal patterns
    - Replace with parentheses for more mathematically correct appearance
    - Restrict all operations to math environment only
    - Auto-format code
    
    * feat(latex-symbols): add new symbols and improve existing ones for readability
    
    - Add several new symbols
    - Improve old symbols for better readability
    
    * feat(latex-font): extend text commands with support for external packages
    
    - Expand list of text commands, including support for external packages
    - Each command now hides only where it actually works
    - Tested on examples
    
    * feat(latex-math): always conceal `\sqrt` braces as parentheses
    
    - Square root command now consistently replaces {} with () for conceal
    
    * feat(latex-symbols): add  popular `\text<...>` symbols
    
    * feat(latex-fonts): Add unicode-math font commands + glyph hiding for `\sym...`, `\math...`, `\bm`
    
    - Added commands (≈40) from the `unicode-math` package for full font support, including aliases and fallbacks for legacy commands.
    
    - Added support for hiding Latin and Greek letters as commands for the `\sym...` symbolic command family.
    
    - For `\math...` commands — hiding support for Latin letters only (no Greek).
    
    - Added hiding for Latin and Greek letters for the `\bm` command to ensure compatibility with pdflatex.
    
    - All glyphs were thoroughly tested across multiple files to ensure visual consistency with compiled documents.

--- lua/math-conceal/symbols/latex/conceal_font.scm
+++ lua/math-conceal/symbols/latex/conceal_font.scm
@@ -1,40 +1,131 @@
 (generic_command
-  command: (command_name) @conceal
-  (#any-of? @conceal "\\emph" "\\mathit" "\\textit" "\\mathbf" "\\textbf" "\\mathbb" "\\mathcal" "\\mathfrak" "\\mathscr" "\\mathsf" "\\mathrm")
-  (#set! conceal ""))
-
-((generic_command
   command: (command_name) @tex_font_name
   arg: (curly_group
-    "{" @left_paren
-    (_)
-    "}" @right_paren))
-  (#any-of? @tex_font_name "\\emph" "\\mathit" "\\textit" "\\mathbf" "\\textbf" "\\mathbb" "\\mathcal" "\\mathfrak" "\\mathscr" "\\mathsf" "\\mathrm")
+    [
+      "{" @left_paren
+      "}" @right_paren
+    ])
+  (#any-of? @tex_font_name
+    "\\bfseries" "\\emph" "\\itshape" "\\mdseries" "\\rmfamily" "\\scshape" "\\sffamily" "\\slshape"
+    "\\textbf" "\\textit" "\\textlf" "\\textmd" "\\textnormal" "\\textrm" "\\textsc" "\\textsf"
+    "\\textsl" "\\texttc" "\\texttt" "\\textulc" "\\textup" "\\ttfamily" "\\upshape")
+  (#not-has-ancestor? @tex_font_name displayed_equation inline_formula math_environment)
   (#set! @left_paren conceal "")
   (#set! @right_paren conceal "")
   (#set! @tex_font_name conceal ""))
 
-((generic_command
+(generic_command
+  command: (command_name) @tex_font_name
+  arg: (curly_group
+    [
+      "{" @left_paren
+      "}" @right_paren
+    ])
+  (#any-of? @tex_font_name
+    "\\bfseries" "\\emph" "\\itshape" "\\mdseries" "\\rmfamily" "\\scshape" "\\sffamily" "\\slshape"
+    "\\textbf" "\\textit" "\\textlf" "\\textmd" "\\textnormal" "\\textrm" "\\textsc" "\\textsf"
+    "\\textsl" "\\texttc" "\\texttt" "\\textulc" "\\textup" "\\ttfamily" "\\upshape")
+  (#has-ancestor? @tex_font_name text_mode)
+  (#set! @left_paren conceal "")
+  (#set! @right_paren conceal "")
+  (#set! conceal ""))
+
+(generic_command
+  command: (command_name) @tex_font_name
+  (#any-of? @tex_font_name
+    "\\Huge" "\\LARGE" "\\Large" "\\footnotesize" "\\huge" "\\large" "\\normalfont" "\\normalsize"
+    "\\scriptsize" "\\small" "\\tiny")
+  (#not-has-ancestor? @tex_font_name displayed_equation inline_formula math_environment)
+  (#set! @tex_font_name conceal ""))
+
+(generic_command
+  command: (command_name) @tex_font_name
+  (#any-of? @tex_font_name
+    "\\Huge" "\\LARGE" "\\Large" "\\footnotesize" "\\huge" "\\large" "\\normalfont" "\\normalsize"
+    "\\scriptsize" "\\small" "\\tiny")
+  (#has-ancestor? @tex_font_name text_mode)
+  (#set! conceal ""))
+
+(text_mode
+  command: "\\text" @tex_font_name
+  content: (curly_group
+    [
+      "{" @left_paren
+      "}" @right_paren
+    ])
+  (#has-ancestor? @tex_font_name displayed_equation inline_formula math_environment)
+  (#set! @left_paren conceal "")
+  (#set! @right_paren conceal "")
+  (#set! @tex_font_name conceal ""))
+
+(text_mode
+  command: [
+    "\\intertext"
+    "\\shortintertext"
+  ] @tex_font_name
+  content: (curly_group
+    [
+      "{" @left_paren
+      "}" @right_paren
+    ])
+  (#has-ancestor? @tex_font_name math_environment)
+  (#set! @left_paren conceal "")
+  (#set! @right_paren conceal "")
+  (#set! @tex_font_name conceal ""))
+
+; full math-font commans list. Work with pdflatex and unicode-math
+(generic_command
+  command: (command_name) @tex_font_name
+  arg: (curly_group
+    [
+      "{" @left_paren
+      "}" @right_paren
+    ])
+  (#any-of? @tex_font_name
+    "\\mathnormal" "\\mathrm" "\\mathit" "\\mathbf" "\\mathsf" "\\mathtt" "\\mathcal" "\\mathscr"
+    "\\mathbb" "\\mathfrak" "\\bm" "\\symup" "\\symit" "\\symbf" "\\symsf" "\\symtt" "\\symbfup"
+    "\\symbfit" "\\symsfup" "\\symsfit" "\\symbfsf" "\\symbfsfup" "\\symbfsfit" "\\symcal"
+    "\\symbfcal" "\\symscr" "\\symbfscr" "\\symbb" "\\symbbit" "\\symfrak" "\\symbffrak" "\\mathup"
+    "\\mathbfup" "\\mathbfit" "\\mathsfup" "\\mathsfit" "\\mathbfsf" "\\mathbfsfup" "\\mathbfsfit"
+    "\\mathbfcal" "\\mathbfscr" "\\mathbbit" "\\mathbffrak")
+  (#has-ancestor? @tex_font_name displayed_equation inline_formula math_environment)
+  (#not-has-ancestor? @tex_font_name text_mode)
+  (#set! @left_paren conceal "")
+  (#set! @right_paren conceal "")
+  (#set! @tex_font_name conceal ""))
+
+; some commands are alias from unicode-math but also work with pdflatex. Conceal only with latin letters
+(generic_command
   command: (command_name) @tex_font_name
   arg: (curly_group
     "{" @left_paren
     (text
       word: (word) @font_letter)
-    "}" @right_paren))
-  (#any-of? @tex_font_name "\\mathbb" "\\mathcal" "\\mathfrak" "\\mathscr" "\\mathsf" "\\mathrm")
+    "}" @right_paren)
+  (#has-ancestor? @tex_font_name displayed_equation inline_formula math_environment)
+  (#not-has-ancestor? @tex_font_name text_mode)
+  (#any-of? @tex_font_name
+    "\\mathrm" "\\mathit" "\\mathbf" "\\mathsf" "\\mathtt" "\\mathcal" "\\mathscr" "\\mathbb"
+    "\\mathfrak" "\\mathup" "\\mathbfup" "\\mathbfit" "\\mathsfup" "\\mathsfit" "\\mathbfsf"
+    "\\mathbfsfup" "\\mathbfsfit" "\\mathbfcal" "\\mathbfscr" "\\mathbbit" "\\mathbffrak")
   (#set! @left_paren conceal "")
   (#set! @right_paren conceal "")
   (#set! @tex_font_name conceal "")
   ; Regex removed - Rust hash table will filter valid font letters
   (#set-font! @font_letter @tex_font_name))
 
-((generic_command
+; conceal letters and greek letters commands
+(generic_command
   command: (command_name) @tex_font_name
   arg: (curly_group
     "{" @left_paren
     (_) @font_letter
-    "}" @right_paren))
-  (#any-of? @tex_font_name "\\bar" "\\widetilde" "\\hat" "\\dot" "\\ddot")
+    "}" @right_paren)
+  (#any-of? @tex_font_name
+    "\\bar" "\\widetilde" "\\hat" "\\dot" "\\ddot" "\\bm" "\\symup" "\\symit" "\\symbf" "\\symsf"
+    "\\symtt" "\\symbfup" "\\symbfit" "\\symsfup" "\\symsfit" "\\symbfsf" "\\symbfsfup"
+    "\\symbfsfit" "\\symcal" "\\symbfcal" "\\symscr" "\\symbfscr" "\\symbb" "\\symbbit" "\\symfrak"
+    "\\symbffrak")
   (#set! @left_paren conceal "")
   (#set! @right_paren conceal "")
   (#set! @tex_font_name conceal "")
--- lua/math-conceal/symbols/latex/conceal_script.scm
+++ lua/math-conceal/symbols/latex/conceal_script.scm
@@ -1,3 +1,29 @@
+; conceal only in math
+; fancy conceal any x^{a+b} or y_{c+d} to x^(a+b) or y_(c+d)
+(subscript
+  subscript: (curly_group
+    [
+      "{" @open_paren
+      "}" @close_paren
+    ]) @sub_object
+  (#has-ancestor? @sub_object displayed_equation inline_formula math_environment)
+  (#not-has-ancestor? @sub_object text_mode)
+  (#set! @open_paren conceal "(")
+  (#set! @close_paren conceal ")"))
+
+(superscript
+  superscript: (curly_group
+    [
+      "{" @open_paren
+      "}" @close_paren
+    ]) @sup_object
+  (#has-ancestor? @sup_object displayed_equation inline_formula math_environment)
+  (#not-has-ancestor? @sup_object text_mode)
+  (#set! @open_paren conceal "(")
+  (#set! @close_paren conceal ")"))
+
+; do not conceal symbols after uderscore in textmode \label{LABEL_WITH_2NUMBER}
+;                                                                 this ^
 (subscript
   "_" @sub_symbol
   subscript: (curly_group
@@ -5,6 +31,8 @@
     (_) @sub_letter
     "}" @close_paren) @sub_object
   (#match? @sub_object "^\\{[aehijklmnoprstuvx1234567890]\\}$")
+  (#has-ancestor? @sub_symbol displayed_equation inline_formula math_environment)
+  (#not-has-ancestor? @sub_symbol text_mode)
   (#set! priority 101)
   (#set! @sub_symbol conceal "")
   (#set! @open_paren conceal "")
@@ -13,18 +41,10 @@
 
 (subscript
   "_" @sub_symbol
-  subscript: (curly_group
-    "{" @open_paren
-    (_)
-    "}" @close_paren) @sub_object
-  (#match? @sub_object "^\\{\\\\\\S+\\}")
-  (#set! @open_paren conceal "")
-  (#set! @close_paren conceal ""))
-
-(subscript
-  "_" @sub_symbol
   subscript: (_) @sub_object
   (#match? @sub_object "^[aehijklmnoprstuvx1234567890]$")
+  (#has-ancestor? @sub_symbol displayed_equation inline_formula math_environment)
+  (#not-has-ancestor? @sub_symbol text_mode)
   (#set! @sub_symbol conceal "")
   (#set-sub! @sub_object))
 
@@ -34,6 +54,8 @@
     "{" @open_paren
     (_) @sup_letter
     "}" @close_paren) @sup_object
+  (#has-ancestor? @sup_symbol displayed_equation inline_formula math_environment)
+  (#not-has-ancestor? @sup_symbol text_mode)
   (#match? @sup_object "^\\{[a-z0-9]\\}$")
   (#set! @sup_symbol conceal "")
   (#set! @open_paren conceal "")
@@ -42,16 +64,143 @@
 
 (superscript
   "^" @sup_symbol
+  superscript: (_) @sup_object
+  (#has-ancestor? @sup_symbol displayed_equation inline_formula math_environment)
+  (#not-has-ancestor? @sup_symbol text_mode)
+  (#match? @sup_object "^[a-z0-9]$")
+  (#set! @sup_symbol conceal "")
+  (#set-sup! @sup_object))
+
+; conceal when simple command, text mode(`\text` command)  or word(one or more letters)
+; do not conceal if inner \frac{a}{b} that make wrong a_a/b or a^a/b with simple expression
+; Complex expression in sub- or superscript is very rare
+; Thats why a^((1+a2+b)) or a_((1+a2+b)) are allowable
+(superscript
+  "^" @sup_symbol
   superscript: (curly_group
     "{" @open_paren
-    "}" @close_paren) @sup_object
-  (#match? @sup_object "^\\{\\\\\\S+\\}$")
+    .
+    (generic_command
+      (command_name) @_frac_name)
+    .
+    (text
+      word: [
+        (subscript
+          subscript: (letter) @sub_letter)
+        (superscript
+          superscript: (letter) @sup_letter)
+      ] .)?
+    .
+    "}" @close_paren)
+  (#has-ancestor? @sup_symbol displayed_equation inline_formula math_environment)
+  (#not-has-ancestor? @sup_symbol text_mode)
+  (#not-match? @_frac_name "^\\\\[dtc]?frac$")
+  (#match? @sup_letter "^[a-z0-9]$")
+  (#match? @sub_letter "^[aehijklmnoprstuvx1234567890]$")
   (#set! @open_paren conceal "")
   (#set! @close_paren conceal ""))
 
 (superscript
   "^" @sup_symbol
-  superscript: (_) @sup_object
-  (#match? @sup_object "^[a-z0-9]$")
-  (#set! @sup_symbol conceal "")
-  (#set-sup! @sup_object))
+  superscript: (curly_group
+    "{" @open_paren
+    .
+    [
+      (text
+        .
+        word: (word) @first_letter
+        .
+        word: [
+          (subscript
+            subscript: (letter) @sub_letter)
+          (superscript
+            superscript: (letter) @sup_letter)
+        ] .)
+      ((text_mode)
+        .
+        (text
+          word: [
+            (subscript
+              subscript: (letter) @sub_letter)
+            (superscript
+              superscript: (letter) @sup_letter)
+          ] .) .)
+      (text
+        .
+        word: (word) @first_letter .)
+      (text_mode)
+    ]
+    .
+    "}" @close_paren)
+  (#has-ancestor? @sup_symbol displayed_equation inline_formula math_environment)
+  (#not-has-ancestor? @sup_symbol text_mode)
+  (#match? @first_letter "^[a-zA-Z0-9]$")
+  (#match? @sup_letter "^[a-z0-9]$")
+  (#match? @sub_letter "^[aehijklmnoprstuvx1234567890]$")
+  (#set! @open_paren conceal "")
+  (#set! @close_paren conceal ""))
+
+(subscript
+  "_" @sub_symbol
+  subscript: (curly_group
+    "{" @open_paren
+    .
+    (generic_command
+      (command_name) @_frac_name)
+    .
+    (text
+      word: [
+        (subscript
+          subscript: (letter) @sub_letter)
+        (superscript
+          superscript: (letter) @sup_letter)
+      ] .)?
+    .
+    "}" @close_paren)
+  (#has-ancestor? @sub_symbol displayed_equation inline_formula math_environment)
+  (#not-has-ancestor? @sub_symbol text_mode)
+  (#not-match? @_frac_name "^\\\\[dtc]?frac$")
+  (#match? @sup_letter "^[a-z0-9]$")
+  (#match? @sub_letter "^[aehijklmnoprstuvx1234567890]$")
+  (#set! @open_paren conceal "")
+  (#set! @close_paren conceal ""))
+
+(subscript
+  "_" @sub_symbol
+  subscript: (curly_group
+    "{" @open_paren
+    .
+    [
+      (text
+        .
+        word: (word) @first_letter
+        .
+        word: [
+          (subscript
+            subscript: (letter) @sub_letter)
+          (superscript
+            superscript: (letter) @sup_letter)
+        ] .)
+      ((text_mode)
+        .
+        (text
+          word: [
+            (subscript
+              subscript: (letter) @sub_letter)
+            (superscript
+              superscript: (letter) @sup_letter)
+          ] .) .)
+      (text
+        .
+        word: (word) @first_letter .)
+      (text_mode)
+    ]
+    .
+    "}" @close_paren)
+  (#has-ancestor? @sub_symbol displayed_equation inline_formula math_environment)
+  (#not-has-ancestor? @sub_symbol text_mode)
+  (#match? @first_letter "^[a-zA-Z0-9]$")
+  (#match? @sup_letter "^[a-z0-9]$")
+  (#match? @sub_letter "^[aehijklmnoprstuvx1234567890]$")
+  (#set! @open_paren conceal "")
+  (#set! @close_paren conceal ""))
