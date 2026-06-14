e269f9de3a1fa517a2ddcd96d8751eacc83ebbd1
yorik1984 <yorik1984@gmail.com>
2026-06-14 22:17:20 +0300

    feat(typst-font): add all possible patterns
    - Add all font patternt for accent and shape variations
    - Tested on source files

--- lua/math-conceal/symbols/typst/conceal_font.scm
+++ lua/math-conceal/symbols/typst/conceal_font.scm
@@ -1,57 +1,52 @@
-; Typst font style conceals - regex removed, Rust will filter
-; Bold math symbols
 (call
-  item: (ident) @typ_font_name
+  item: [
+    (ident)
+    (field)
+  ] @typ_font_name
   (#any-of? @typ_font_name
-    "bold" "italic" "cal" "script" "bb" "sans" "mono" "frak" "double" "upright" )
-  (#set! @typ_font_name conceal "")
-  "(" @left_paren
-  (#set! @left_paren conceal "")
-  (formula) @font_letter
-  (#set-font! @font_letter @typ_font_name "font")
-  ")" @right_paren
-  (#set! @right_paren conceal ""))
-
-; overline conceal for ident - regex removed, Rust will filter
-(call
-  item: (_) @typ_font_name
-  (#any-of? @typ_font_name "overline" "tilde" "hat" "dot" "dot.double")
-  (#set! @typ_font_name conceal "")
-  "(" @left_paren
-  (#set! @left_paren conceal "")
-  (formula) @font_letter
-  (#set-font! @font_letter @typ_font_name "font")
-  ")" @right_paren
-  (#set! @right_paren conceal "")
-  (#set! priority 102))
-
-(call
-  item: (_) @typ_font_name
-  (#any-of? @typ_font_name "overline")
-  (#set! @typ_font_name conceal "")
-  "(" @left_paren
-  (#set! @left_paren conceal "")
-  (formula) @font_letter
-  (#set-font! @font_letter @typ_font_name "font")
-  ")" @right_paren
-  (#set! @right_paren conceal "")
-  (#set! priority 102))
-
-; Math function calls with special symbols
-(call
-  item: (ident) @typ_math_font
-  (#any-of? @typ_math_font "dif")
-  ; (#has-ancestor? @func math formula)
-  (#set! conceal "d"))
-
-(((ident) @typ_math_font
-  (#any-of? @typ_math_font "dif"))
-  ; (#has-ancestor? @conceal math formula)
-  (#set! @typ_math_font "d"))
-
-; Script functions like upright, script, etc.
-(call
-  item: (ident) @func
-  (#any-of? @func "upright" "italic" "script" "mono" "sans")
-  (#has-ancestor? @func math formula)
+    "bb" "bold" "cal" "frak" "italic" "mono" "sans" "scr" "serif" "upright" "acute" "acute.double"
+    "arrow" "arrow.l" "arrow.l.r" "breve" "caron" "circle" "dash" "diaer" "dot" "dot.double"
+    "dot.quad" "dot.triple" "grave" "harpoon" "harpoon.lt" "hat" "macron" "overline" "tilde")
+  (#has-ancestor? @typ_font_name math formula)
+  (#not-has-parent? @typ_font_name field)
   (#set! conceal ""))
+
+(call
+  item: [
+    (ident)
+    (field)
+  ] @typ_font_name
+  "(" @left_paren
+  (formula) @font_letter
+  ")" @right_paren
+  (#any-of? @typ_font_name
+    "bb" "bold" "cal" "frak" "italic" "mono" "sans" "scr" "serif" "upright" "acute" "acute.double"
+    "arrow" "arrow.l" "arrow.l.r" "breve" "caron" "circle" "dash" "diaer" "dot" "dot.double"
+    "dot.quad" "dot.triple" "grave" "harpoon" "harpoon.lt" "hat" "macron" "overline" "tilde")
+  (#has-ancestor? @typ_font_name math formula)
+  (#not-has-parent? @typ_font_name field)
+  (#set! @typ_font_name conceal "")
+  (#set! @left_paren conceal "")
+  (#set! @right_paren conceal "")
+  (#set-font! @font_letter @typ_font_name))
+
+; digits in font
+(call
+  item: [
+    (ident)
+    (field)
+  ] @typ_font_name
+  "(" @left_paren
+  (formula) @font_digit
+  ")" @right_paren
+  (#any-of? @typ_font_name
+    "bb" "bold" "cal" "frak" "italic" "mono" "sans" "scr" "serif" "upright" "acute" "acute.double"
+    "arrow" "arrow.l" "arrow.l.r" "breve" "caron" "circle" "dash" "diaer" "dot" "dot.double"
+    "dot.quad" "dot.triple" "grave" "harpoon" "harpoon.lt" "hat" "macron" "overline" "tilde")
+  (#has-ancestor? @typ_font_name math formula)
+  (#not-has-parent? @typ_font_name field)
+  (#lua-match? @font_digit "^%d+$")
+  (#set! @left_paren conceal "")
+  (#set! @typ_font_name conceal "")
+  (#set! @right_paren conceal "")
+  (#set-font! @font_digit @typ_font_name))
