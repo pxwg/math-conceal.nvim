664f77a1cb6762466e8a55fcfd12ec182bd51ba2
yorik1984 <yorik1984@gmail.com>
2026-06-22 15:32:12 +0300

    feat(typst-math): update `scm`
    - Add `sqrt`, `root`,  `vec`, `mat`,  `binom`, `limits`
    - Add special math symbols and operators hl
    - Update `flac` rulers

--- lua/math-conceal/symbols/typst/conceal_math.scm
+++ lua/math-conceal/symbols/typst/conceal_math.scm
@@ -5,9 +5,63 @@
   (ident)
   (field)
 ] @typ_math_symbol
-  (#not-has-parent? @typ_math_symbol field call)
+  (#not-has-parent? @typ_math_symbol field call tagged)
   (#has-ancestor? @typ_math_symbol math)
   (#set-conceal! @typ_math_symbol "conceal"))
+
+(call
+  item: (ident) @typ_math_symbol
+  (#any-of? @typ_math_symbol "sqrt")
+  (#has-ancestor? @typ_math_symbol math)
+  (#set-conceal! @typ_math_symbol "conceal"))
+
+(call
+  item: (ident) @typ_math_symbol
+  "("
+  .
+  (formula
+    (number) @_number .)
+  ")"
+  (#eq? @typ_math_symbol "root")
+  (#not-any-of? @_number "3" "4")
+  (#has-ancestor? @typ_math_symbol math)
+  (#set-conceal! @typ_math_symbol "conceal"))
+
+(call
+  item: (ident) @typ_math_symbol
+  "("
+  .
+  (formula
+    (number) @_number_3 .)
+  ")"
+  (#eq? @typ_math_symbol "root")
+  (#eq? @_number_3 "3")
+  (#has-ancestor? @typ_math_symbol math)
+  (#set! @typ_math_symbol conceal "∛"))
+
+(call
+  item: (ident) @typ_math_symbol
+  "("
+  .
+  (formula
+    (number) @_number_4 .)
+  ")"
+  (#eq? @typ_math_symbol "root")
+  (#eq? @_number_4 "4")
+  (#has-ancestor? @typ_math_symbol math)
+  (#set! @typ_math_symbol conceal "∜"))
+
+([
+  (ident)
+  (field)
+] @typ_symbol
+  (#any-of? @typ_symbol
+    "Im" "Re" "degree" "ell" "percent" "permille" "permyriad" "planck" "prime" "prime.double"
+    "prime.double.rev" "prime.quad" "prime.rev" "prime.rev.double" "prime.rev.triple" "prime.triple"
+    "prime.triple.rev" "angstrom")
+  (#has-ancestor? @typ_symbol math)
+  (#not-has-parent? @typ_symbol field call tagged)
+  (#set-conceal! @typ_symbol "conceal"))
 
 ; #sym.<>
 ((code
@@ -15,6 +69,17 @@
     (_))) @typ_math_symbol
   (#match? @typ_math_symbol "^([#]sym[.])\\S+$")
   (#set-sym-conceal! @typ_math_symbol "conceal"))
+
+([
+  (ident)
+  (field)
+] @symbol
+  (#any-of? @symbol
+    "dot" "dot.basic" "dot.c" "dot.op" "dots" "dots.c" "dots.c.h" "dots.down" "dots.h" "dots.h.c"
+    "dots.up" "dots.v")
+  (#has-ancestor? @symbol math)
+  (#not-has-parent? @symbol field call tagged)
+  (#set-conceal! @symbol "conceal"))
 
 ((shorthand) @symbol
   (#has-ancestor? @symbol math)
@@ -30,7 +95,7 @@
   (#has-ancestor? @typ_symbol math)
   (#set-conceal! @typ_symbol "conceal"))
 
-; conceal brackets and comma in tables, grid etc.
+; conceal brackets and comma in tables, grid etc. Also remove all comma in other commands
 ([
   "," @punctuation.delimiter
   (content
@@ -48,19 +113,51 @@
   (field)
 ] @typ_symbol
   (#any-of? @typ_symbol "dif" "partial")
-  (#not-has-parent? @typ_symbol field call)
+  (#not-has-parent? @typ_symbol field call tagged)
   (#has-ancestor? @typ_symbol math)
   (#set-conceal! @typ_symbol "conceal"))
 
+; conceal command(content) -> (content)
 (call
   item: (ident) @cmd
   "(" @left_brace
   ")" @right_brace
   (#has-ancestor? @cmd math formula)
-  (#eq? @cmd "vec")
+  (#any-of? @cmd "vec" "mat" "binom")
   (#set! @cmd conceal "")
   (#set! @left_brace conceal "(")
   (#set! @right_brace conceal ")"))
+
+; conceal command(var1,var2) -> (var1 var2)
+(call
+  item: (ident) @cmd
+  "," @punctuation.delimiter
+  (#any-of? @cmd "vec" "mat" "binom")
+  (#has-ancestor? @cmd math formula)
+  (#set! @punctuation.delimiter conceal " "))
+
+; conceal command(content) -> content
+(call
+  item: (ident) @cmd
+  "(" @left_brace
+  (_)
+  ")" @right_brace
+  (#has-ancestor? @cmd math formula)
+  (#any-of? @cmd "limits")
+  (#set! @cmd conceal "")
+  (#set! @left_brace conceal "")
+  (#set! @right_brace conceal ""))
+
+((call
+  item: (ident) @cmd
+  "("
+  .
+  (formula
+    (_) @symbol)
+  ")")
+  (#has-ancestor? @cmd math formula)
+  (#eq? @cmd "limits")
+  (#set-conceal! @symbol "conceal"))
 
 (call
   item: (ident) @typ_symbol
@@ -76,13 +173,25 @@
   (#set! @frac conceal "∕"))
 
 ; Conceal "frac" and replace with opening parenthesis
-; only for simple expression frac(expression_1,expression_2)
-; to expression_1/expression_2
+; for simple expression frac(expression_1,expression_2)
+; to (expression_1⧸expression_2)
 ; or for long_expression frac((long_expression_1),(long_expression_2))
-; to (long_expression_1)/(long_expression_2)
+; to (long_expression_1)∕(long_expression_2)
 ; Replace only first comma with division slash
 ; frac(content,content,style: str)
 ; use `⧸` BIG SOLIDUS U+29F8
+(call
+  item: (ident) @_frac_name
+  "("
+  .
+  (_)
+  .
+  "," @punctuation.comma
+  ")"
+  (#eq? @_frac_name "frac")
+  (#set! @punctuation.comma conceal "")
+  (#set! @_frac_name conceal ""))
+
 (call
   item: (ident) @_frac_name
   (#eq? @_frac_name "frac")
@@ -119,29 +228,22 @@
       .
       (group) .)
   ]
-  .
-  (","
-    .
-    (tagged))?
   ")" @right_paren
-  (#set! @punctuation.comma conceal "")
+  (#set! @punctuation.comma conceal "∕")
   (#set! @left_paren conceal "")
   (#set! @right_paren conceal "")
   (#set! @_frac_name conceal ""))
 
-; Conceal style for all frac functions
-; from frac(content,content,style: str) to frac(content,content)
+; Conceal style for allfunctions
+; from cmd(content,content,style: str) to cmd(content,content)
 (call
   item: (ident) @_func_name
-  (#eq? @_func_name "frac")
-  (_)
-  .
-  ","
-  .
-  (_)
-  .
+  (#any-of? @_func_name "frac")
+  "("
   "," @punctuation.comma
   .
   (tagged) @_tagged
+  .
+  ")"
   (#set! @punctuation.comma conceal "")
   (#set! @_tagged conceal ""))
